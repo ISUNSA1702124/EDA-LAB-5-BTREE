4c01f1cd8eb5ff6d1f91177f4b093e3e5afc88e9
Milton <milton761@gmail.com>
2021-11-23 11:55:04 -0500

    - kick off

---
+++ src/main/java/datastructures/BTree.java
@@ -0,0 +1,33 @@
+package datastructures;
+
+public class BTree<E extends Comparable<? super E>> implements BTreeInterface<E> {
+
+    @Override
+    public boolean add(E value) {
+        //TODO implement here!
+        return false;
+    }
+
+    @Override
+    public E remove(E value) {
+        //TODO implement here!
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        //TODO implement here!
+    }
+
+    @Override
+    public boolean contains(E value) {
+        //TODO implement here!
+        return false;
+    }
+
+    @Override
+    public int size() {
+        //TODO implement here!
+        return 0;
+    }
+}
---
+++ src/main/java/exercise/Exercise.java
@@ -0,0 +1,8 @@
+package exercise;
+
+public class Exercise {
+    public String allTrees(Integer n) {
+        //TODO implement here!
+        return null;
+    }
+}
